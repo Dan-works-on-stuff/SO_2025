56b58a444053d4be0557c2b7392a516eb8cfb176
Radu Dan-Stefan <154246298+MaXCoX610@users.noreply.github.com>
2025-04-12 19:54:35 +0300

    lab7 al fut

--- lab7/peco.c
+++ lab7/peco.c
@@ -2,7 +2,6 @@
    Iată o soluție incompletă, în sensul că s-a omis codul efectiv de punere și de scoatere a lacătului pentru acces exclusiv la fișierul de date partajat,
    precum și tratarea anumitor excepții (i.e., atunci când se încearcă extragerea unei cantități mai mari decât cea disponibilă în rezervorul stației PECO).
 */
-// TODO: respectivele omisiuni vă rămân ca temă să le completați dvs. !!!
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,20 +12,50 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void initializare_fisier_date();
+void initializare_fisier_date(char *filename) {
+	int fd= creat(filename, 0600);
+	if (fd==-1) {
+		perror("creat failed");
+	}
+	srandom( getpid() );
+	float qty=random()%100;
+	if (-1== write (fd, &qty, sizeof(int))) {
+		perror("write failed");
+	}
+	close(fd);
+}
 /*
 *  Pentru început, avem nevoie de o rutină ajutătoare, care să creeze conținutul inițial al fișierului de date partajat.
 *  Se va crea fișierul și va conține un număr real, stocat în format binar, reprezentând cantitatea inițială de combustibil din rezervor.
 *  Vom apela programul cu opțiunea -i pentru a invoca această rutină de inițializare.
 */
 
-void afisare_fisier_date();
+void afisare_fisier_date(char *filename) {
+	float qty;
+	int bytes_read;
+	int fd = open(filename, O_RDONLY);
+	struct flock lock;
+	if (fd==-1) {
+		perror("open failed");
+		exit(1);
+	}
+	lock.l_type   = F_RDLCK;
+	lock.l_whence = SEEK_CUR;
+	lock.l_start  = SEEK_SET;
+	lock.l_len    = SEEK_END;
+	if(read(fd, &qty, sizeof(int)) == -1){
+		perror("read failed");
+		exit(2);
+	}
+	printf("%f\n", qty);
+	close(fd);
+}
 /*
 *  Similar, la finalul tuturor actualizărilor, vom avea nevoie de o altă rutină ajutătoare, care să ne afișeze conținutul final din rezervor.
 *  Vom apela programul cu opțiunea -o pentru a invoca această rutină de afișare finală.
 */
 
-void secventa_de_actualizari(int n, char* nr[]);
+void secventa_de_actualizari(int n, char* nr[]) ;
 /*
 *  Rutina principală a programului, care va executa secvența de actualizări primită în linia de comandă, în manieră concurentă,
 *  conform celor specificate în enunțul problemei.
@@ -44,62 +73,20 @@ int main(int argc, char* argv[])
 	/* Cazul de inițializare a fișierului de date. */
 	if( strcmp(argv[1],"-i") == 0 )
 	{
-		initializare_fisier_date();  return 0;
+		initializare_fisier_date("peco.bin");
+		return 0;
 	}
 
 	/* Cazul de afișare a fișierului de date. */
 	if( strcmp(argv[1],"-o") == 0 )
 	{
-		afisare_fisier_date();  return 0;
+		afisare_fisier_date("peco.bin");
+		return 0;
 	}
 
 	/* Cazul general: actualizarea fișierului de date. */
 	secventa_de_actualizari(argc, argv);
 	return 0;
-}
-
-
-void initializare_fisier_date()
-{/* Funcționalitate: inițializarea fișierului de date. */
-
-	int fd;
-	float val;
-
-	printf("Dati cantitatea initiala de combustibil (i.e., un numar real pozitiv): ");
-	while( (1 != scanf("%f", &val)) && (val < 0) ) { fprintf(stderr,"Eroare: nu ati introdus un numar real pozitiv! Incercati din nou..."); }
-
-	if(-1 == (fd = creat("peco.bin", 0600) ) )
-	{
-		perror("Eroare la crearea fisierului de date...");  exit(2);
-	}
-
-	if(-1 == write(fd, &val, sizeof(float) ) )
-	{
-		perror("Eroare la scrierea valorii initiale in fisierul de date...");  exit(3);
-	}
-
-	close(fd);
-}
-
-
-void afisare_fisier_date()
-{/* Funcționalitate: afișarea fișierului de date. */
-
-	int fd;
-	float val;
-
-	if ( -1 == (fd = open("peco.bin", O_RDONLY) ) )
-	{
-		perror("Eroare la deschiderea pentru afisare a fisierului de date...");  exit(4);
-	}
-
-	if (-1 == read(fd, &val, sizeof(float) ) )
-	{
-		perror("Eroare la citirea valorii finale din fisierul de date...");  exit(5);
-	}
-
-	close(fd);
-	printf("Stocul final de combustibil este: %f litri de combustibil.\n", val);
 }
 
 
@@ -111,7 +98,8 @@ void secventa_de_actualizari(int n, char* nr[])
 
 	if ( -1 == (fd = open("peco.bin", O_RDWR) ) )
 	{
-		perror("Eroare la deschiderea pentru actualizare a fisierului de date...");  exit(6);
+		perror("Eroare la deschiderea pentru actualizare a fisierului de date...");
+		exit(6);
 	}
 
 	srandom( getpid() ); //  Inițializarea generatorului de numere aleatoare.
@@ -125,9 +113,11 @@ void secventa_de_actualizari(int n, char* nr[])
 		}
 
 		/* Începutul secțiunii critice! */
-		/*
-			... // TODO:  implementați singuri punerea lacătului în scriere pe fișier.
-		*/
+		struct flock lock;
+		lock.l_type=F_WRLCK;
+		lock.l_whence=SEEK_CUR;
+		lock.l_start=SEEK_SET;
+		lock.l_len= SEEK_END;
 
 		if(-1 == lseek(fd, 0, SEEK_SET) ) // (Re)poziționăm cursorul la începutul fișierului.
 		{
@@ -141,12 +131,12 @@ void secventa_de_actualizari(int n, char* nr[])
 		printf("[PID: %d] Se adauga/extrage in/din rezervor cantitatea de %f litri de combustibil.\n", getpid(), valoare );
 
 		stoc = stoc + valoare;
-		/*
-			... // TODO: implementați singuri tratarea excepției: stoc negativ
-			... // Varianta 1: refuzarea operației de extragere, cu un mesaj informativ în acest sens.
-			... // Varianta 2: realizarea unei extrageri parțiale, i.e. se extrage doar cât e în stoc, nu cât s-a cerut, cu un mesaj informativ în acest sens.
-			... // Varianta 3: interogarea utilizatorului, ce variantă de rezolvare dorește? , i.e. varianta 1 sau varianta 2 de mai sus?
-		*/
+		if (stoc < 0) {
+			float surplus = stoc;
+			stoc=0;
+			printf("Cantitatea ceruta nu a putut fi supplied, alimentat doar %f", valoare+surplus);
+		}
+		// Varianta 2: realizarea unei extrageri parțiale, i.e. se extrage doar cât e în stoc, nu cât s-a cerut, cu un mesaj informativ în acest sens.
 
 		if(-1 == lseek(fd, 0, SEEK_SET) ) // Repoziționăm cursorul la începutul fișierului.
 		{
@@ -158,10 +148,10 @@ void secventa_de_actualizari(int n, char* nr[])
 		}
 
 		/* Sfârșitul secțiunii critice! */
-		/*
-			... // TODO: implementați singuri scoaterea lacătului de pe fișier.
-		*/
-
+		lock.l_type=F_UNLCK;
+		if (fcntl(fd, F_SETLK, &lock) == -1) {
+			perror("deblocare");
+		}
 		sleep( random() % 5 ); // Facem o pauză aleatoare, de maxim 4 secunde, înainte de a continua cu următoarea iterație.
 	}
 
