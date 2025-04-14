e6bf315ef7e28f9460d8ba304539d4dd54382683
Radu Dan-Stefan <154246298+Dan-works-on-stuff@users.noreply.github.com>
2025-04-14 12:35:10 +0300

    da

--- lab6/my_ls.c
+++ lab6/my_ls.c
@@ -9,20 +9,37 @@
 
 int main(int argc, char *argv[]) {
     int argument = 0;
-
-    if (argc < 2) {
-        argument = 0;
-    } else if (argv[1][0] == '-' && argv[1][1] == 'A') {
-        argument = 1;
-    } else if (argv[1][0] == '-' && argv[1][1] == 'l') {
-        argument = 2;
-    } else if (argv[1][0] == '-' && argv[1][1] == 'a') {
-        argument = 3;
-    }
     DIR *dir = opendir(".");
     if (dir == NULL) {
         perror("opendir");
     }
+    if (argc < 2 || argv[1][0] == '.' && argv[1][1] == '\0') {
+        argument = 0;
+    } else if (argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 'A':
+                argument = 1;
+            break;
+            case 'l':
+                argument = 2;
+            break;
+            case 'a':
+                argument = 3;
+            break;
+            default:
+                printf("Unknown option\n");
+            break;
+        }
+    } else {
+        if (strcmp(argv[1], "..") == 0) {
+            closedir(dir); // Close the initial current directory
+            dir = opendir("..");
+            if (dir == NULL) {
+                perror("opendir");
+            }
+        }
+    }
+
     struct dirent *entry;
 
     if (argument == 0) {
